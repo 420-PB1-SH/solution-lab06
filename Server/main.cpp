505862725943cc8891cb1d96e60d8ce49e719db7
Pier-Luc Brault <pier-luc@brault.me>
2023-02-22 15:33:21 -0500

    Working tic tac toe

--- Server/main.cpp
+++ Server/main.cpp
@@ -9,38 +9,6 @@ int main()
 {
 	TicTacToe ticTacToe;
 
-	cout << ticTacToe;
-
-	cout << "Gagnant: " << ticTacToe.getGagnant() << endl;
-	cout << "Match nul: " << ticTacToe.estMatchNul() << endl;
-
-	ticTacToe.jouer(0, 0, 'x');
-
-	cout << ticTacToe;
-
-	cout << "Gagnant: " << ticTacToe.getGagnant() << endl;
-	cout << "Match nul: " << ticTacToe.estMatchNul() << endl;
-
-	ticTacToe.jouer(0, 0, 'o');
-
-	cout << ticTacToe;
-
-	cout << "Gagnant: " << ticTacToe.getGagnant() << endl;
-	cout << "Match nul: " << ticTacToe.estMatchNul() << endl;
-
-	ticTacToe.jouer(0, 1, 'x');
-
-	cout << ticTacToe;
-
-	cout << "Gagnant: " << ticTacToe.getGagnant() << endl;
-	cout << "Match nul: " << ticTacToe.estMatchNul() << endl;
-
-	ticTacToe.jouer(0, 2, 'x');
-
-	cout << ticTacToe;
-
-	cout << "Gagnant: " << ticTacToe.getGagnant() << endl;
-	cout << "Match nul: " << ticTacToe.estMatchNul() << endl;
 
 	system("pause");
 }
